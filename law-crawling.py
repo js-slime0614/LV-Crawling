1915f742a631dd5f704041bb5e847d04c8a8f14d
js-slime0614 <qkrwnstj010@naver.com>
2023-11-09 16:03:30 +0900

    국가법령정보센터에서 크롤링

--- law-crawling.py
+++ law-crawling.py
@@ -1,51 +1,67 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import time
 
-#드라이버 지정
+# 드라이버 지정
 webDriver = webdriver.Chrome()
 
-#법제처 국가법령정보센터 사이트열기
-base_url = 'https://www.law.go.kr/lsSc.do?menuId=1&subMenuId=15&tabMenuId=81#undefined'
+# 법제처 국가법령정보센터 사이트열기
+base_url = 'https://www.law.go.kr/expcSc.do?menuId=7&subMenuId=51&tabMenuId=237&query=#AJAX'
 webDriver.get(base_url)
 webDriver.implicitly_wait(10) #로딩 끝날때까지 10초 기다리기
-
-#사이트 접속후 pdf 저장을 위해 리스트 밀어넣기
-webDriver.find_element(By.CSS_SELECTOR, 'div.westClose').click()
-
-webDriver.find_element(By.CSS_SELECTOR, '#listDiv > div.paging > a:nth-child(5)').click()
 time.sleep(5)
 
-webDriver.find_element(By.CSS_SELECTOR,'#listDiv > div.paging > ol > li:nth-child(3) > a').click()
-time.sleep(5)
+# 크롤링중 끊겼을시 끊긴위치로 이동
+for i in range(0, 26): # 반복문으로 최적화 처리
+    webDriver.find_element(By.CSS_SELECTOR, '#listDiv > div.paging > a:nth-child(4)').click()
+    time.sleep(3)
+    i += 1
+webDriver.find_element(By.CSS_SELECTOR,'#listDiv > div.paging > ol > li:nth-child(2) > a').click()
 
-pageNum = 2
-
+# 현재 페이지 반영
+# Current Page 132 from
+pageNum = 3
+checknum = 1
 while(1) :
-    #법령 본문 넘기기 위해정의
-    contentNum = 0
+    
+    time.sleep(10)
+    contents = webDriver.find_elements(By.CSS_SELECTOR, 'ul.left_list_bx > li > a')
+    for content in contents:
+        try : 
+            # 원하는부분에서 크롤링 하기 위해 넘기기
+            if checknum <= 0:
+                checknum += 1
+                continue
 
-    #해당법령클릭후 pdf 저장까지 구현
-    while(1) :
-        if contentNum >= 50: break
-        webDriver.find_element(By.CSS_SELECTOR, '#liBgcolor' + str(contentNum) + ' > a').click()
-        time.sleep(10)
-        webDriver.find_element(By.CSS_SELECTOR, '#bdySaveBtn').click()
-        time.sleep(5)
-        webDriver.find_element(By.CSS_SELECTOR, 'input#FileSavePdf1.radio').click()
-        webDriver.find_element(By.CSS_SELECTOR,'a#aBtnOutPutSave').click()
-        time.sleep(5)
-        contentNum += 1
+            content.click()
+            webDriver.implicitly_wait(100)
+            time.sleep(20)
+            
+            webDriver.find_element(By.CSS_SELECTOR, '#bdySaveBtn').click()
+            webDriver.implicitly_wait(20)
+            time.sleep(5)
+            webDriver.find_element(By.CSS_SELECTOR, 'select#EmpDocSelectId').click()
+            webDriver.find_element(By.CSS_SELECTOR,'#EmpDocSelectId > option:nth-child(2)').click()
+            webDriver.find_element(By.CSS_SELECTOR,'input#empSaveBtn').click()
+            webDriver.implicitly_wait(20) 
+            time.sleep(5)
 
-    #5단위 페이지 넘기기
+        except:
+            print("Get Exception")
+            continue
+    
+    # 5단위 페이지 넘기기
     if pageNum >= 6 :
+        time.sleep(10)
         webDriver.find_element(By.CSS_SELECTOR, '#listDiv > div.paging > a:nth-child(4)').click()
+        webDriver.implicitly_wait(20) 
         time.sleep(5)
         pageNum = 2
 
-    #페이지 넘기기
+    # 페이지 넘기기
     else :
+        time.sleep(10)
         webDriver.find_element(By.CSS_SELECTOR,'#listDiv > div.paging > ol > li:nth-child('+ str(pageNum) +') > a').click()
+        webDriver.implicitly_wait(20) 
         time.sleep(5)
         pageNum += 1
