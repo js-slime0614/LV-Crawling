f6b2ffe7aedb82a37ec6681dbba80da486bec407
js-slime0614 <qkrwnstj010@naver.com>
2023-11-16 17:27:19 +0900

    국가법령정보센터에서 이혼분야 판례검색을 통해 크롤링

--- lawOpenData.py
+++ lawOpenData.py
@@ -1,7 +1,10 @@
 import requests
 import json
-import xml
 import xml.etree.ElementTree as ET
+import csv
+import re
+
+to_crawling_jo_num = 931
 
 def send_api(path, method, type):
     API_HOST1 = "http://www.law.go.kr/DRF/lawSearch.do?"
@@ -21,13 +24,33 @@ def send_api(path, method, type):
         elif method == 'POST':
             response = requests.post(url, headers=headers, data=json.dumps(body, ensure_ascii=False, indent="\t"))
         print("response status %r" % response.status_code)
-        print("response text %r" % response.text)
+        # print("response text %r" % response.text)
         return response.text
     except Exception as ex:
         print(ex)
-        
-# 호출 예시
-api = send_api("OC=parkjs0052&target=law&type=XML&ID=001706&JO=083400", "GET", "service")
-"djEJgrp qhauscka wjswodrkxdkTekdnfl godqhrgks skskfdp dnffuTejs akfeh aksgdkTdj rlfdjTejs tlrksdmf wlzuwnjTes sjdi sjfmf sjantkfkdgotj rmrp ajfdjwlrp gksrjfRk djfltnrgks sowkfahtdlsrk tkfkddl tlrdjTekrh thfwlrgl akfgoeheho rktmaEnlsms tjffpadl sprp~ dhsrjfkrh skqhek whgdmstkfkadl sjdprp todrlsrjfkrh ekfmsvldrPrpTwl dnflsdksakwsmsekrh wlfflrp emfdjTdj slakadl Ejskrksgnfh ghrtlsk dnflrk rhoscksgdms tlwjfdp akssk wkfgownjTekausdms dnflwhrmadms ekfmfRk ghdndnn sjfmf sjantkfkdgotj rmrp ajfdjwlrpgksrjfRK cjfdldjqtsms sodyrtladlsrk tkfkddl tlrdjTekrh thfwlrgl akfgoeheho rktmaEnlsms tjffpadl sorp dhsrjfkrh skqhek whgdmstkfkadl sjdprp todrlsrjfkrh dkclaRkakgehfhr cnlgo dlwdmfu skfakdrkEMfueh skadkdlTsmssj skqhekej djelrk whgdktj Ejsksrjsl dlwdjqhfu goqhkeh aldnj sorkwhaej wkfsktj tjdrhdgkfEoWma sldkvdptj qhfrp rmEosjs ghffh dlrlfmf"
 
-root = ET.fromstring(api)
+# 호출 예시
+# email @ 뒤 제외, 민법내 조 
+def crawling_law_from_civil(user_email, jo_num):
+    api = send_api("OC=" + user_email + "&target=law&type=XML&ID=001706&JO=" + str(jo_num), "GET", "service")
+    print("%r" % api)
+    root = re.findall('CDATA.*]', api)
+    print("%r" % root)
+    return_data = ''
+    for content in root:
+        content = content.replace('CDATA', '')
+        content = content.replace('[', '')
+        content = content.replace(']', '')
+        return_data = return_data + ' ' + content
+    print(return_data)
+    append_csv(return_data)
+    return return_data
+
+# 데이터 csv 파일로 저장
+f = open('민법' + str(to_crawling_jo_num) +'조항.csv', 'w', encoding='utf-8', newline='')
+wr = csv.writer(f)
+def append_csv(append_string):
+    wr.writerow([append_string])
+   
+data = crawling_law_from_civil('parkjs0052','0' + str(to_crawling_jo_num) + '00') 
+f.close()
